b0d81792c3546e101c54727044266570955e4762
JasserMbareck <jasserMbareck@gmail.com>
2023-12-15 23:46:40 +0100

    initiaal commit

---
+++ db/actualites.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 15 déc. 2023 à 23:12
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `newsss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actualites`
+--
+
+CREATE TABLE `actualites` (
+  `id` int(20) NOT NULL,
+  `titre` varchar(80) NOT NULL,
+  `description` text NOT NULL,
+  `date_pub` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `actualites`
+--
+ALTER TABLE `actualites`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `actualites`
+--
+ALTER TABLE `actualites`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- server.js
+++ server.js
@@ -7,29 +7,55 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '',
-  database : 'news'
+  database : 'newsss'
 });
 
 connection.connect();
 
+var exphbs = require('express-hbs');
+app.engine('hbs', exphbs.express4({
+  partialsDir: __dirname + '/views'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+  const sql = 'select * from actualites ORDER BY date_pub DESC';
+  connection.query(sql, (error,results,fields) =>{
+    if (error){
+      console.error('error fetching news :' + error.stack);
+      res.status(500).send('Internal server Error');
+     return;
+    }
+      res.render('home' ,{actualites: results});
+    
+  });
 });
 
-app.get("/add", (req, res) => {
+app.get("/add",(req, res) => {
   res.sendFile(path.join(__dirname, "views/add.html"));
 });
 
 app.get("/addnews", function(req, res) {
     var untitre = req.query.letitre;
     var unedesc = req.query.ladescription;
-    var sql = "insert into actualites(titre, description) values(?, ?)"
+    if (!untitre || !unedesc) {
+      res.send('<script>alert("Title and description are required!"); window.location="/add";</script>');
+      return; 
+  }
+    var sql = "insert into actualites(titre, description) values(?, ?)";
     
     connection.query(sql, [untitre, unedesc], function(error, results, fields) {
-        res.send(results)
-    })
+       if (error) {
+         console.error("error inserting data into the database", error);
+         res.status(500).send("Internal server error");
+       }else{
+        res.redirect('/');
+       }
+    });
 });
 
 app.listen(port, () => {
